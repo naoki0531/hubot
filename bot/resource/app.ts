4449ebb2c4f554c550a0034dcd7cf0a6a0ae8b0d
丸山　直樹 <uu106892@CA1750.local>
2018-03-09 13:59:27 +0900

    add date arguments

--- bot/resource/app.ts
+++ bot/resource/app.ts
@@ -7,15 +7,33 @@ interface birthday {
 }
 
 module.exports = (robot: hubot.Robot): void => {
-    robot.respond(/hello/i, (msg: hubot.Response) => {
-        const today: Date = new Date();
-        const ymd: string = String(today.getFullYear()) + ('00' + (today.getMonth() + 1)).slice(-2) + ('00' + today.getDate()).slice(-2);
+    robot.respond(/誕生日( *)(.*)/i, (msg: hubot.Response) => {
+
+        msg.reply(msg.match[2]);
+
+        let compareDate = '';
+        let dateMessage = '本日';
+        if (msg.match[2] !== '') {
+            compareDate = msg.match[2];
+            dateMessage = msg.match[2];
+        } else {
+            compareDate = createTodayString();
+        }
+
         const birthdayList: Array<birthday> = JSON.parse(fs.readFileSync('data/birthday.json', 'utf8'));
+        let message = `${dateMessage}が誕生日の方はいません。`;
         birthdayList.forEach((birthday: birthday) => {
-            if (ymd === birthday.date) {
-                msg.reply(`本日は${birthday.name}さんの誕生日です！`);
+            if (compareDate === birthday.date) {
+                message = `${dateMessage}は${birthday.name}さんの誕生日です！`;
                 return;
             }
         });
+
+        msg.reply(message);
     });
+
+    function createTodayString(): string {
+        const today: Date = new Date();
+        return ('00' + (today.getMonth() + 1)).slice(-2) + ('00' + today.getDate()).slice(-2);
+    }
 };
